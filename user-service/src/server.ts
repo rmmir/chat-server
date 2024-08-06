7e57e0e883c505a70749bb3761a713b3c1e2e2af
radumiron12 <radumiron12@gmail.com>
2024-08-07 00:00:03 +0300

    file refactoring

--- user-service/src/server.ts
+++ user-service/src/server.ts
@@ -1,5 +1,4 @@
 import express from 'express'
-import { Server } from 'http'
 import userRouter from './routes/authRoutes'
 import { errorConverter, errorHandler } from './middleware'
 import { connectToDB } from './database'
